0ada9a80c6120a25e821e21567a984bbe9809d39
Tim Konick <konick781@gmail.com>
2015-05-14 23:20:18 -0400

    made an adjustment to handling input

--- main.c
+++ main.c
@@ -368,7 +368,7 @@ void action_help() {
 		"\tl - loot container\n"
 		"\tf - fight boss\n"
 		"\tp - put on armor\n"
-		"\tW - weild a weapon\n"
+		"\tW - wield a weapon\n"
 		"\tE - eat food\n"
 		"\tr - reflect upon myself\n"
 		"\tx - examine\n"
@@ -382,10 +382,9 @@ void action_help() {
 
 //tutorial intro sequence
 void tutorial(dungeon* d) {
-	d->player.name = calloc(1, MAX_NAME_LENGTH);
 	int x;
 
-	input_string((char*)d->player.name, MAX_NAME_LENGTH - 1, "What is my name?");
+	d->player.name = input_string("What is my name?");
 	for (x=0; x<TR_NUM; ++x) {
 		printf("[%d] %s\n", x, adj_trait[x]);
 	}
